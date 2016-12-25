f5e32bda105c7bd27a4465ee95f5c95fbdc7df00
wuhyn <hong.yang.ng@gmail.com>
2016-12-25 20:11:14 +1100

    Added color changes

--- module-one/app.js
+++ module-one/app.js
@@ -11,32 +11,35 @@
   LunchCheckController.$inject = ['$scope', '$filter', '$injector'];
 
   function LunchCheckController($scope, $filter, $injector){
+    $scope.dishes = "";
+    $scope.colorChange = "black";
 
     $scope.checkDishes = function (){
       //Get the number of dishes and store it in an array using split()
       var numOfDishes = $scope.dishes.split(",");
       var len = numOfDishes.length;
-      console.log(len);
+      //console.log(len);
 
       //Checks if "dishes" textbox is empty or filled
+      //Test condition $scope.dishes.length == 1
       if($scope.dishes == ""){
         $scope.result = "Please enter data first";
+        //console.log("Empty state");
+        $scope.colorChange = "red";
       }
-
+      
       //Check for less or equal to 3 elements
       else if(len <= 3){
         $scope.result = "Enjoy!";
+        $scope.colorChange = "green";
       }
 
       //Check for more 3
       else if(len > 3){
         $scope.result = "Too much!"
+        $scope.colorChange = "green";
       }
 
-      //else condition to change "result" variable to empty string
-      else{
-        $scope.result = "";
-      }
     }
 
     //Annotate textbox, ubtton, placeholder
