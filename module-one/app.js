5cc3e7451538405eecf169a7344f1655525c97b0
wuhyn <hong.yang.ng@gmail.com>
2016-12-25 20:39:38 +1100

    Minor Fixes

--- module-one/app.js
+++ module-one/app.js
@@ -1,43 +1,55 @@
 //Follow IEFE
 (function (){
   'use strict';
+  
   //Create Angular Module
   angular.module('LunchCheck', [])
 
-  //Controller for handling textbox, the button, and
-  //the message placeholder
+  //Controller for handling textbox, the button, and the message placeholder
   .controller('LunchCheckController', LunchCheckController);
 
   LunchCheckController.$inject = ['$scope', '$filter', '$injector'];
 
   function LunchCheckController($scope, $filter, $injector){
     $scope.dishes = "";
+    $scope.borderColor = "";
     $scope.colorChange = "black";
 
     $scope.checkDishes = function (){
       //Get the number of dishes and store it in an array using split()
+      //comma as the delimiter
       var numOfDishes = $scope.dishes.split(",");
+      //len for testing number of dishes
       var len = numOfDishes.length;
       //console.log(len);
 
-      //Checks if "dishes" textbox is empty or filled
+      //Checks for space or empty string
       //Test condition $scope.dishes.length == 1
-      if($scope.dishes == ""){
+      if($scope.dishes == "" || $scope.dishes == " "){
+        //Enter data message
         $scope.result = "Please enter data first";
         //console.log("Empty state");
+        //Text and border color changes
         $scope.colorChange = "red";
-      }
-      
-      //Check for less or equal to 3 elements
-      else if(len <= 3){
-        $scope.result = "Enjoy!";
-        $scope.colorChange = "green";
+        $scope.borderColor = "red";
       }
 
-      //Check for more 3
-      else if(len > 3){
-        $scope.result = "Too much!"
+      //Check for less or equal to 3 items
+      else if(len <= 3){
+        //Enjoy Message
+        $scope.result = "Enjoy!";
+        //Text and border color changes
         $scope.colorChange = "green";
+        $scope.borderColor = "green";
+      }
+
+      //Check for more than 3 items
+      else if(len > 3){
+        //Too much message
+        $scope.result = "Too much!"
+        //Text and border color changes
+        $scope.colorChange = "green";
+        $scope.borderColor = "green";
       }
 
     }
