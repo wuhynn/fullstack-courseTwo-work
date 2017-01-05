bae7ff6978885ae7b140d7c5d84ba6f454186f17
wuhyn <hong.yang.ng@gmail.com>
2017-01-06 04:05:28 +1100

    update todolist and user relationships

--- module-two-sol/db/schema.rb
+++ module-two-sol/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105162715) do
+ActiveRecord::Schema.define(version: 20170105165536) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 20170105162715) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "TodoList_id"
   end
+
+  add_index "users", ["TodoList_id"], name: "index_users_on_TodoList_id"
 
 end
