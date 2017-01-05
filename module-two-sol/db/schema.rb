4f3f07acd69c098f2468605ba16f4abb5de7ff28
wuhyn <hong.yang.ng@gmail.com>
2017-01-06 06:26:42 +1100

    req 6 met

--- module-two-sol/db/schema.rb
+++ module-two-sol/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105165536) do
+ActiveRecord::Schema.define(version: 20170105192203) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 20170105165536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "todo_items", force: :cascade do |t|
+    t.date     "due_date"
+    t.string   "title"
+    t.text     "description"
+    t.boolean  "completed"
+    t.integer  "todo_list_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "list_name"
