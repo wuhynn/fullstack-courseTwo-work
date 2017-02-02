78c2efe03275b5e1f85eb9779ac13f9f08701f28
wuhyn <hong.yang.ng@gmail.com>
2017-02-03 04:46:16 +1100

    rq10 added

--- module-four-sol/app/controllers/sessions_controller.rb
+++ module-four-sol/app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #get username and pw from form_for
+    user = User.find_by(username: params[:user][:username])
+    password = params[:user][:password]
+
+    if user && user.authenticate(password)
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      redirect_to login_path, alert: "Invalid username/password combination"
+    end
   end
 
   def destroy
+    reset_session #wipe everything
+    redirect_to login_path, notice: "You have been logged out"
   end
 end
